d672003c532d79073aae642dc0f6745623cbd1e2
issa1999 <73497948+issa1999@users.noreply.github.com>
2020-12-26 20:46:44 +0100

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,17 @@
 
+import math 
+def digPow(n,p):
+   int h=0:
+    l=[]
+    for i in range(len(str(n))):
+        l[i]=int(str(n)[i])
+    
+    for j in range(len(l)):
+        h+=pow(l[j],j+p)
+    if h%n!=0:
+        return -1
+    else :
+        return h/n
+
+        
+    
